be9e3e806390acdc990c714897f8bca218288a61
sun_of_rain <sy2481@126.com>
2019-07-08 15:07:12 +0800

    update

--- ChatroomServerPart/src/main/java/com/sunzhouyu/server/core/Entity.java
+++ ChatroomServerPart/src/main/java/com/sunzhouyu/server/core/Entity.java
@@ -1,31 +1,36 @@
 package com.sunzhouyu.server.core;
+
 /**
  * <servlet>
-  <servlet-name>login</servlet-name>
-  <servlet-class>com.shsxt.LoginServlet</servlet-class>
-  </servlet>
- * 
- * @author 裴新 QQ:3401997271
+ * <servlet-name>login</servlet-name>
+ * <servlet-class>com.shsxt.LoginServlet</servlet-class>
+ * </servlet>
  *
+ * @author 裴新 QQ:3401997271
  */
 public class Entity {
     private String name;
+
     private String clz;
+
     public Entity() {
-		// TODO Auto-generated constructor stub
-	}
+        // TODO Auto-generated constructor stub
+    }
+
     public String getName() {
-		return name;
-	}
+        return name;
+    }
+
     public void setName(String name) {
-		this.name = name;
-	}
+        this.name = name;
+    }
+
     public String getClz() {
-		return clz;
-	}
+        return clz;
+    }
+
     public void setClz(String clz) {
-		this.clz = clz;
-	}
-	
-	
+        this.clz = clz;
+    }
+
 }
--- ChatroomServerPart/src/main/java/com/sunzhouyu/server/core/Servlet.java
+++ ChatroomServerPart/src/main/java/com/sunzhouyu/server/core/Servlet.java
@@ -1,10 +1,10 @@
 package com.sunzhouyu.server.core;
+
 /**
  * 服务器小脚本接口
- * 
- * @author 裴新 QQ:3401997271
  *
+ * @author 裴新 QQ:3401997271
  */
 public interface Servlet {
-	void service(Request request, Response response);
+    void service(Request request, Response response);
 }
